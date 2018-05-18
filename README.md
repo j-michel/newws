20c7c184a7a3c00a64b958f180e5e46fec440d61
Jean <jean@windie.co>
2018-05-18 22:11:56 +0200

    WIP

---
+++ README.md
@@ -0,0 +1,5 @@
+Launch server : php bin/console server:start
+
+Extract articles : ./bin/console app:extract:articles
+
+Deploy application : ./bin/console deploy
---
+++ src/Provider/MediapartProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Provider;
+
+use Symfony\Component\DomCrawler\Crawler;
+use Goutte\Client;
+
+class MediapartProvider extends BaseProvider
+{
+
+  protected function getItemDescription($item)
+  {
+    $crawler = new Crawler($item->getDescription());
+
+    return $crawler->filter('p')->first()->text();
+  }
+
+  protected function getPublishedAt($item)
+  {
+    return $item->getLastModified();
+  }
+
+  protected function getItemCoverUrl($item)
+  {
+    $client = new Client();
+
+    $crawler = $client->request('GET', $item->getLink());
+
+    return $crawler->filterXPath("//meta[@property='og:image']")->first()->attr('content');
+  }
+
+}
