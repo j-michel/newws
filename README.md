fc9199fe3089e67a66994aec3d528859f3ce0d43
Jean <jean@windie.co>
2018-06-02 17:49:04 +0200

    More videos

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Launch server : php bin/console server:start
 Extract articles : ./bin/console app:extract:articles
 
 Deploy application : ./bin/console deploy
+
+-----
+
+update bdd : ./bin/console doctrine:schema:update --force
---
+++ src/Provider/OlgProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Provider;
+
+use Symfony\Component\DomCrawler\Crawler;
+use Goutte\Client;
+
+class OlgProvider extends BaseProvider
+{
+  protected function getItemDescription($item)
+  {
+    return "";
+  }
+
+  protected function getPublishedAt($item)
+  {
+    foreach ($item->getAllElements() as $element) {
+
+      if($element->getName() == "published"){
+
+        return new \DateTime($element->getValue());
+      }
+    }
+  }
+
+  protected function getItemCoverUrl($item)
+  {
+    foreach ($item->getAllElements() as $element) {
+
+      if($element->getName() == "media:group"){
+
+          foreach ($element->getAllElements() as $mediaElement) {
+            if($mediaElement->getName() == "media:thumbnail"){
+
+              $attributes = $mediaElement->getAttributes();
+              return $attributes['url'];
+            }
+          }
+        }
+      }
+      return null;
+  }
+
+  protected function getType($item)
+  {
+    return 'video';
+  }
+}
